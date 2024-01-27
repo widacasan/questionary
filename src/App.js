6809fb713e18cecbcc57016440d6616b98bc80de
William David Camacho Sanchez <will@MacBook-Air-de-William.local>
2024-01-27 15:57:08 -0500

    create Intro / Home Screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const HomeScreen = ({ startQuiz }) => (
+  <div>
+    <h1>Welcome to the Trivia Challenge!</h1>
+    <p>You will be presented with 10 true or false questions.</p>
+    <p>Can you store 100%?</p>
+    <button onClick={startQuiz}>BEGIN</button>
+  </div>
+);
+
+const QuizScreen = ({ questions }) => (
+  <div>
+    <h1>Trivia Questions</h1>
+    {questions.map((question, index) => (
+      <div key={index}>
+        <h3>Pregunta {index + 1}</h3>
+        <p>Categoría: {question.category}</p>
+        <p>Dificultad: {question.difficulty}</p>
+        <p>Pregunta: {question.question}</p>
+        <p>Respuesta Correcta: {question.correct_answer}</p>
+        <p>Respuesta Incorrecta: {question.incorrect_answers}</p>
+        <hr />
+      </div>
+    ))}
+  </div>
+);
+
+const App = () => {
+  const [questions, setQuestions] = useState([]);
+  const [quizStarted, setQuizStarted] = useState(false);
+
+  const startQuiz = async () => {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean');
+      const data = await response.json();
+      setQuestions(data.results);
+      setQuizStarted(true);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {quizStarted ? (
+        <QuizScreen questions={questions} />
+      ) : (
+        <HomeScreen startQuiz={startQuiz} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
